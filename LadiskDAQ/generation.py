f5b0dde3d37c142cfbe3690c92999a1abc2cf371
klemengit <zaletelj.klemen@gmail.com>
2022-06-08 12:47:37 +0200

    init generation

---
+++ LadiskDAQ/generation.py
@@ -0,0 +1,29 @@
+import os
+import numpy as np
+
+from .daqtask import DAQTask
+
+class BaseGenerator:
+    def __init__(self):
+        self.is_running = True
+
+    def generate(self):
+        pass
+
+    def run_generation(self):
+        while self.is_running:
+            self.generate()
+
+
+class NIGenerator(BaseGenerator):
+    def __init__(self, task_name, signal):
+        super().__init__()
+        self.task_name = task_name
+        self.Task = DAQTask(self.task_name)
+        self.signal = signal
+    
+    def generate(self):
+        self.Task.generate(self.signal, clear_task=False)
+
+    def clear_task(self):
+        self.Task.clear_task(wait_until_done=False)

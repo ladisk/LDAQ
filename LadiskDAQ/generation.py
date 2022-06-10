5e06df4082ebefe7795f87d19262529ea675c28a
klemengit <zaletelj.klemen@gmail.com>
2022-06-10 07:07:07 +0200

    generation update

--- LadiskDAQ/generation.py
+++ LadiskDAQ/generation.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import time
 
 from .daqtask import DAQTask
 
@@ -14,6 +15,15 @@ class BaseGenerator:
         while self.is_running:
             self.generate()
 
+    def set_data_source(self):
+        pass
+
+    def clear_data_source(self):
+        pass
+
+    def stop(self):
+        self.is_running = False
+
 
 class NIGenerator(BaseGenerator):
     def __init__(self, task_name, signal):
@@ -21,13 +31,29 @@ class NIGenerator(BaseGenerator):
         self.task_name = task_name
         self.Task = DAQTask(self.task_name)
         self.signal = signal
+
+    def set_data_source(self):
+        if not hasattr(self, 'Task'):
+            self.Task = DAQTask(self.task_name)
+        
+    def clear_data_source(self):
+        self.clear_task()
     
     def generate(self):
         self.Task.generate(self.signal, clear_task=False)
 
     def clear_task(self):
         self.Task.clear_task(wait_until_done=False)
+        del self.Task
 
-    def stop(self):
-        self.clear_task()
-        self.is_running = False
+    def run_generation(self):
+        self.set_data_source()
+
+        self.generate()
+
+        while self.is_running:
+            print('checking')
+            time.sleep(0.5)
+        
+        self.clear_data_source()
+        

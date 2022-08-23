6ab3673dbd49d0ee719d3cb9039d7917281c4514
TKosir <tilen.kosir96@gmail.com>
2022-08-23 08:43:09 +0200

    added serial connection acquisition

--- LadiskDAQ/acquisition.py
+++ LadiskDAQ/acquisition.py
@@ -3,6 +3,8 @@ import numpy as np
 import pickle
 import datetime
 import time
+import serial
+import struct
 
 from PyDAQmx.DAQmxFunctions import *
 from PyDAQmx.Task import Task
@@ -18,6 +20,10 @@ class BaseAcquisition:
         self.channel_names = []
         self.plot_data = []
         self.is_running = True
+
+        # child class needs to have variables below:
+        self.n_channels = 0
+        self.sample_rate = 0
 
     def read_data(self):
         """EDIT in child class
@@ -94,7 +100,7 @@ class BaseAcquisition:
     def set_trigger_instance(self):
         self.Trigger = pyTrigger(
             rows=self.trigger_settings['duration_samples'], 
-            channels=self.Task.number_of_ch,
+            channels=self.n_channels,
             trigger_type=self.trigger_settings['type'],
             trigger_channel=self.trigger_settings['channel'], 
             trigger_level=self.trigger_settings['level'],
@@ -139,8 +145,139 @@ class ADAcquisition(BaseAcquisition):
 
 
 class SerialAcquisition(BaseAcquisition):
-    def __init__(self, port_nr):
-        super.__init__()
+    """General Purpose Class for Serial Communication"""
+    def __init__(self, port, baudrate, byte_sequence, start_bytes=b"\xfa\xfb", end_bytes=b"\n", 
+                       write_bytes=None, channel_names = None, sample_rate=1):
+        """
+        Initializes serial communication.
+
+        :param: port - (str) serial port (i.e. "COM1")
+        :param: baudrate - (int) baudrate for serial communication
+        :param: byte_sequence - (tuple) data sequence in each recived line via serial communication
+                                example: (("int16", 2), ("int32", 2), ("uint16", 3))
+                                explanations: line consists of 2 16bit signed intigers, followed by 
+                                2 signed 32bit intigers, followed by 3 unsigned 16bit intigers.
+
+                                supported types: int8, uint8, int16, uint16, int32, uint32
+        :param: start_bytes - (bstr) received bytes via serial communication indicating the start of each line
+        :param: end_bytes   - (bstr) recieved bytes via serial communication indicating the end of each line
+        :param: write_bytes - (tuple) sequence of b"" strings with bytes to write to initiate/set data transfer or other settings
+                              Writes each encoded bstring with 1ms delay.
+        :param: sample_rate - approximate sample rate of incoming signal - currently needed only for plot purposes
+                                
+                            
+        """
+        super().__init__()
+
+        self.port = port
+        self.baudrate = baudrate
+        self.byte_sequence = byte_sequence
+        self.write_bytes = write_bytes
+        self.start_bytes = start_bytes
+        self.end_bytes = end_bytes
+
+        self.unpack_string = b""
+        self.expected_number_of_bytes = 0
+        self.n_channels = 0
+        self.channel_names = channel_names
+
+        self.set_unpack_data_settings() # sets unpack_string, expected_number_of_bytes, n_channels
+        self.set_channel_names()        # sets channel names if none were given to the class
+        self.set_data_source()          # initializes serial connection
+    
+        self.sample_rate = sample_rate   # TODO: estimate sample_rate  automatically
+        self.buffer = b""                # buffer to which recieved data is added
+
+        # set default trigger, so the signal will not be trigered:
+        self.set_trigger(1e20, 0, duration=600)
+
+    def clear_data_source(self):
+        return self.ser.close()
+
+    def read_data(self):
+        # 1) read all data from serial
+        self.buffer += self.ser.read_all()
+
+        # 2) split data into lines
+        parsed_lines = self.buffer.split(self.end_bytes + self.start_bytes)
+        if len(parsed_lines) == 1 or len(parsed_lines) == 0: # not enough data
+            return np.array([]).reshape(-1, self.n_channels)
+        
+        # 3) decode full lines, convert data to numpy array
+        data = []
+        for line in parsed_lines[:-1]: # last element probably does not contain all data
+            if len(line) == self.expected_number_of_bytes - len(self.end_bytes+self.start_bytes):
+                line_decoded = struct.unpack(self.unpack_string, line)
+                data.append(line_decoded)
+            else:
+                #print(f"Expected nr. of bytes {self.expected_number_of_bytes}, line contains {len(line)}")
+                pass
+        data = np.array(data)
+        if len(data) == 0:
+            data = data.reshape(-1, self.n_channels)
+
+        # 4) reset buffer with remaninig bytes:
+        self.buffer = self.end_bytes + self.start_bytes + parsed_lines[-1]
+
+        return data
+    
+    def set_data_source(self):
+        if not hasattr(self, 'ser'):
+            try:
+                self.ser = serial.Serial(port=self.port, baudrate=self.baudrate)
+            except serial.SerialException:
+                print("Serial port is in use.")
+        elif not self.ser.is_open:
+            self.ser.open()
+        else:
+            pass 
+
+        if self.write_bytes is None:
+            pass
+        else:
+            #TODO: write self.write_bytes sequence
+            pass
+
+        self.ser.read_all() # clears previous data
+        self.ser.read_all()
+        self.ser.read_all()
+
+    def set_unpack_data_settings(self):
+        """
+        Converts byte_sequence to string passed to struct unpack method.
+        """
+        self.convert_dict = {
+            "uint8":  ("B", 1), # (struct format, number of bytes)
+            "int8":   ("b", 1),
+            "uint16": ("H", 2),
+            "int16":  ("h", 2),
+            "uint32": ("L", 4),
+            "int32":  ("l", 4),
+        }
+
+        self.unpack_string = "<" # order of several bytes for 1 variable (see struct library)
+        self.n_channels = 0
+        self.expected_number_of_bytes = len(self.start_bytes) + len(self.end_bytes)
+        for seq in self.byte_sequence:
+            dtype, n = seq
+            for i in range(n):
+                self.unpack_string += self.convert_dict[dtype][0]
+                self.expected_number_of_bytes += self.convert_dict[dtype][1]
+                self.n_channels += 1
+
+        return self.unpack_string
+
+    def set_channel_names(self):
+        """
+        Sets default channel names if none were passed to the class.
+        """
+        if self.channel_names is None:
+            self.channel_names = [f"channel {i+1}" for i in range(self.n_channels)]
+        else:
+            if len(self.channel_names) != self.n_channels:
+                self.channel_names = [f"channel {i+1}" for i in range(self.n_channels)]
+            else:
+                self.channel_names = self.channel_names
 
 
 class NIAcquisition(BaseAcquisition):
@@ -166,6 +303,7 @@ class NIAcquisition(BaseAcquisition):
 
         self.channel_names = self.Task.channel_list
         self.sample_rate = self.Task.sample_rate
+        self.n_channels = self.Task.number_of_ch
 
         # set default trigger, so the signal will not be trigered:
         self.set_trigger(1e20, 0, duration=600)

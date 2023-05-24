76a475831f3f8d63b32cdcc5dbf4aa23195d22bc
TKosir <tilen.kosir96@gmail.com>
2023-05-24 07:48:36 +0200

    basler CAM update

--- LadiskDAQ/acquisition.py
+++ LadiskDAQ/acquisition.py
@@ -35,12 +35,6 @@ try:
 except:
     print("pypylon library not found. Please install using pip install pypylon")
 
-
-class DummyLock:
-    def __enter__(self):
-        return self
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        pass
     
 class CustomPyTrigger(pyTrigger):
     """
@@ -1424,7 +1418,7 @@ class BaslerCamera(BaseAcquisition):
     
     """
     def __init__(self, acquisition_name=None, sample_rate=60, offset=(0, 0), size=(4112, 3008),
-                 subsample=1, pixel_format="Mono12", buffer_dtype=np.uint16):
+                 subsample=1, pixel_format="Mono12", exposure_time_ms=4.0):
         super().__init__()
 
         self.acquisition_name = 'BaslerCamera' if acquisition_name is None else acquisition_name
@@ -1435,24 +1429,27 @@ class BaslerCamera(BaseAcquisition):
         self.size = size # camera size
         self.offset = offset # camera offsets
         self.pixel_format = pixel_format
+        self.exposure_time = exposure_time_ms # in ms
         
-        self.buffer_dtype = buffer_dtype# TODO: adjust this according to pixel_format
+        self.buffer_dtype = np.uint16# TODO: adjust this according to pixel_format
+        self.camera_acq_started = False
         self.set_data_source(start_grabbing=False) # to read self.image_shape
         
         # there will always be only 1 channel and it will always display temperature
         self.n_channels  = self.image_shape[0]*self.image_shape[1]
         self.channel_names = ['Camera']
         
+        
         # channel in set trigger is actually pixel in flatten array:
         self.set_trigger(1e20, 0, duration=1.0)
-        
-        
        
     def set_data_source(self, start_grabbing=True):
         """
         Properly sets acquisition source before measurement is started.
         Should be set up in a way that it is able to be called multiple times in a row without issues.    
         """
+        self.current_image_ID = 0 # reset image_ID
+        
         if hasattr(self, 'camera'):
             pass
         else:
@@ -1461,29 +1458,30 @@ class BaslerCamera(BaseAcquisition):
             print("Using device:", self.camera.GetDeviceInfo().GetModelName())
 
             self.camera.PixelFormat.SetValue(self.pixel_format)  # set pixel depth to 16 bits
-            self.camera.ExposureTime.SetValue(60000.0)  # set exposure time to 60 ms
-            self.camera.OffsetX.SetValue(self.offset[0])  # set the offset x
-            self.camera.OffsetY.SetValue(self.offset[1])  # set the offset y
+            self.camera.ExposureTime.SetValue(self.exposure_time*1000)  # set exposure time 
             self.camera.Width.SetValue(self.size[0])  # set the width
             self.camera.Height.SetValue(self.size[1])  # set the height
+            self.camera.OffsetX.SetValue(self.offset[0])  # set the offset x
+            self.camera.OffsetY.SetValue(self.offset[1])  # set the offset y
             
             
             # Get the node map
             nodemap = self.camera.GetNodeMap()
 
             # Set the acquisition frame rate to 60 fps
+            self.camera.AcquisitionFrameRateEnable.SetValue(True)
             self.camera.AcquisitionFrameRate.SetValue(self.sample_rate)
+            self.camera.MaxNumBuffer = 15
                     
             # Get the image size
             width = self.camera.Width.GetValue()
             height = self.camera.Height.GetValue()
-            
             self.image_shape = ( (np.arange(height)[::self.subsample]).shape[0], (np.arange(width)[::self.subsample]).shape[0]) 
                
-        
         if start_grabbing:
-            self.camera.StartGrabbing(pylon.GrabStrategy_LatestImageOnly)  
-            time.sleep(0.01)
+            self.camera_acq_started = True
+        if self.camera_acq_started:
+            self.camera.StartGrabbing(pylon.GrabStrategy_OneByOne)  
 
     def read_data(self):
         """
@@ -1494,15 +1492,23 @@ class BaslerCamera(BaseAcquisition):
         Must return a 2D numpy array of shape (n_samples, n_columns).
         """
         # Wait for an image and then retrieve it. A timeout of is used.
-        grabResult = self.camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
-        # Image grabbed successfully?
-        
-        if grabResult.GrabSucceeded():
-            image_temp = grabResult.Array[::self.subsample, ::self.subsample]
-            #self.image_shape = image_temp.shape
-            grabResult.Release()
-            return image_temp.reshape(-1, self.n_channels)
-        else:
+        #grabResult = self.camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
+        try:
+            grabResult = self.camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
+            
+            if grabResult.GrabSucceeded():
+                image_temp = grabResult.Array[::self.subsample, ::self.subsample]
+                current_image_ID = grabResult.GetImageNumber()
+                if self.current_image_ID + 1 != current_image_ID:
+                    print("Warning: frames might be missed!")
+                self.current_image_ID = current_image_ID
+                
+                grabResult.Release()
+                return image_temp.reshape(-1, self.n_channels)
+            else:
+                return np.empty((0, self.n_channels))
+            
+        except pylon.TimeoutException:
             return np.empty((0, self.n_channels))
     
     def terminate_data_source(self):
@@ -1512,7 +1518,6 @@ class BaslerCamera(BaseAcquisition):
         Returns None.
         """
         self.camera.StopGrabbing()
-        #grabResult.Release()
         self.camera.Close()
    
     def get_sample_rate(self):

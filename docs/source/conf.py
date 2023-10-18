db9e5da84441710952fbc9e7d70fb1e395bbcd56
klemengit <zaletelj.klemen@gmail.com>
2023-10-18 11:29:49 +0200

    Updated config.py for docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,6 +50,7 @@ extensions = [
 ]
 
 nbsphinx_allow_errors = True
+nbsphinx_execute = 'never'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
